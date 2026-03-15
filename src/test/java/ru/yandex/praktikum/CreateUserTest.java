16c872db6425d1c7f7dcb8213d3e0c0647c2542d
Andrey Shevelev <wild0511@yandex.ru>
2026-03-16 00:12:35 +0700

    Задание 2

--- src/test/java/ru/yandex/praktikum/CreateUserTest.java
+++ src/test/java/ru/yandex/praktikum/CreateUserTest.java
@@ -6,29 +6,48 @@ import org.apache.http.HttpStatus;
 import org.junit.After;
 import org.junit.Test;
 import ru.yandex.praktikum.models.User;
+
 import static org.hamcrest.Matchers.equalTo;
 
 public class CreateUserTest extends BaseTest {
     private String token;
 
     @After
-    public void tearDown() { userClient.deleteUser(token); }
+    public void tearDown() {
+        if (token != null) {
+            userClient.deleteUser(token);
+        }
+    }
 
     @Test
-    @DisplayName("Создание пользователя без имени")
-    public void createUserWithoutName() {
-        User user = new User("ivan" + System.currentTimeMillis() + "@ya.ru", "1234", "");
+    @DisplayName("Создание уникального пользователя")
+    @Description("Успешное создание пользователя со всеми обязательными полями")
+    public void createUniqueUserSuccess() {
+        User user = new User("user_" + System.currentTimeMillis() + "@ya.ru", "pass123", "Andrey");
+        var response = userClient.createUser(user);
+        token = response.path("accessToken");
+
+        response.then().assertThat().statusCode(HttpStatus.SC_OK)
+                .and().body("success", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Создание пользователя без пароля")
+    @Description("Проверка ошибки 403 при отсутствии поля password")
+    public void createUserWithoutPasswordFail() {
+        User user = new User("user_" + System.currentTimeMillis() + "@ya.ru", "", "Andrey");
         userClient.createUser(user)
                 .then().assertThat().statusCode(HttpStatus.SC_FORBIDDEN)
                 .and().body("message", equalTo("Email, password and name are required fields"));
     }
 
     @Test
-    @DisplayName("Создание уникального пользователя")
-    public void createUniqueUser() {
-        User user = new User("ivan" + System.currentTimeMillis() + "@ya.ru", "1234", "Ivan");
-        var resp = userClient.createUser(user);
-        token = resp.path("accessToken");
-        resp.then().assertThat().statusCode(HttpStatus.SC_OK).body("success", equalTo(true));
+    @DisplayName("Создание пользователя без логина (email)")
+    @Description("Проверка ошибки 403 при отсутствии поля email")
+    public void createUserWithoutEmailFail() {
+        User user = new User("", "pass123", "Andrey");
+        userClient.createUser(user)
+                .then().assertThat().statusCode(HttpStatus.SC_FORBIDDEN)
+                .and().body("message", equalTo("Email, password and name are required fields"));
     }
 }
